fdcabef663752b25b8847dc54155d95e62dec8f8
Muhammad Ridwan <ridwanmhmmd18@gmail.com>
2023-01-14 15:20:06 +0700

    Add: authentication

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,9 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'username' => ['required', 'string', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'level' => ['required']
         ]);
     }
 
@@ -66,8 +67,9 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'username' => $data['username'],
             'password' => Hash::make($data['password']),
+            'level' => $data['level'],
         ]);
     }
 }
