eb133fdc2b3af329c8f07d6ea02a4371050c6c2f
Muhammad Ridwan <ridwanmhmmd18@gmail.com>
2023-01-30 20:27:35 +0700

    Add: checkin dapur
    -checkin dapur

--- app/Http/Controllers/DapurController.php
+++ app/Http/Controllers/DapurController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 
 use Illuminate\Http\Request;
+use App\Models\Absen;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use Auth;
 
 class DapurController extends Controller
 {
@@ -78,6 +81,116 @@ class DapurController extends Controller
         );
 
         echo json_encode($res);
+    }
+
+    public function indexAbsenDapur(){
+
+        return view('dapur.absen-dapur');
+    }
+
+    public function fetchAllAbsenDapur()
+    {
+        $id = Auth::user()->id;
+        $emps = Absen::select('absensi.id','users.name','users.level','absensi.created_at','absensi.checkout')
+        ->join('users','users.id','absensi.users_id')
+        ->where('users.id',$id)
+        ->get();
+        $output = '';
+		if ($emps->count() > 0) {
+			$output .= '<table class="table table-striped table-sm text-center align-middle">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Nama</th>
+                <th>Level</th>
+                <th>Check In</th>
+                <th>Check Out</th>
+              </tr>
+            </thead>
+            <tbody>';
+			foreach ($emps as $emp) {
+				$output .= '<tr>
+                <td>' . $emp->id . '</td>
+                <td>' . $emp->name . '</td>
+                <td>' . $emp->level . '</td>
+                <td>' . $emp->created_at . '</td>
+                <td>' . $emp->checkout . '</td>
+              </tr>';
+			}
+			$output .= '</tbody></table>';
+			echo $output;
+		} else {
+			echo '<h1 class="text-center text-secondary my-5">No record present in the database!</h1>';
+		}
+    }
+
+  public function fetchAllAbsenDapurx(Request $request)
+  {
+    {
+      if($request->ajax())
+      {
+       if($request->from_date != '' && $request->to_date != '')
+       {
+        $id = Auth::user()->id;
+        $data = Absen::select('absensi.id','users.name','users.level','absensi.created_at','absensi.checkout')
+        ->join('users','users.id','absensi.users_id')
+        ->where('users.id', $id)
+          ->whereBetween('absensi.created_at', array($request->from_date, $request->to_date))
+          ->get();
+
+          echo json_encode($data);
+       }
+     }
+  }
+}
+
+    public function checkinDapur(){
+        $id = Auth::user()->id;
+        $data = Absen::whereDate('created_at', '=', date('Y-m-d'))
+        ->where('users_id',$id)
+        ->get();
+
+        if(empty($data->count() > 0)){
+            $absen = new Absen();
+            $absen->users_id = $id;
+            $absen->flag_checkin = 1;
+            $absen->flag_checkout = 0;
+            $absen->save();
+
+            return response()->json([
+                'status' => 200,
+            ]);
+        }else
+        {
+            return response()->json([
+                'status' => 400,
+                'msg' => 'Sudah checkin hari ini'
+            ]);
+        }
+    }
+
+    public function checkoutDapur(){
+        $id = Auth::user()->id;
+        $data = Absen::whereDate('created_at', '=', date('Y-m-d'))
+        ->where('users_id',$id)
+        ->where('flag_checkin','1')
+        ->where('flag_checkout','0')
+        ->get();
+
+        if(!empty($data->count() > 0)){
+
+            $update = DB::select('UPDATE absensi SET flag_checkout = 1, checkout = now() WHERE flag_checkout = 0 AND DATE(created_at) = CURDATE() AND users_id = ?',[$id]);
+
+            return response()->json([
+                'status' => 200,
+            ]);
+        }else
+        {
+            return response()->json([
+                'status' => 400,
+                'msg' => 'Sudah checkout hari ini'
+            ]);
+        }
     }
     
 }
---
+++ resources/views/dapur/absen-dapur.blade.php
@@ -0,0 +1,187 @@
+@extends('layouts.menu')
+
+@section('content')
+
+<body class="bg-light">
+  <div class="container">
+  <div class="row input-daterange">
+        <div class="col-md-4">
+            <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" readonly />
+        </div>
+        <div class="col-md-4">
+            <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date" readonly />
+        </div>
+        <div class="col-md-4">
+            <button type="button" name="filter" id="filter" class="btn btn-primary">Filter</button>
+            <button type="button" name="refresh" id="refresh" class="btn btn-default">Refresh</button>
+        </div>
+    </div>
+    <div class="row my-5">
+      <div class="col-lg-12">
+        <div class="card shadow">
+          <div class="card-header bg-danger d-flex justify-content-between align-items-center">
+            <h3 class="text-light">Report Absensi</h3>
+            <form action="#" method="POST" id="add_checkin" enctype="multipart/form-data">
+            @csrf
+            <button type="submit" id="checkin" class="btn btn-primary">Check In</button>
+            </form>
+
+            <form action="#" method="POST" id="add_checkout" enctype="multipart/form-data">
+            @csrf
+            <button type="submit" id="checkout" class="btn btn-primary">Check Out</button>
+            </form>
+          </div>
+          <div class="card-body" id="show_all_absen">
+            <h1 class="text-center text-secondary my-5">Loading...</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.min.js'></script>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.10.25/datatables.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/js/bootstrap-datepicker.js"></script>
+  <script>
+    $(function() {
+
+      $("#add_checkin").submit(function(e) {
+        e.preventDefault();
+        const fd = new FormData(this);
+        $.ajax({
+          url: '{{ route('checkinDapur') }}',
+          method: 'post',
+          data: fd,
+          cache: false,
+          contentType: false,
+          processData: false,
+          dataType: 'json',
+          success: function(response) {
+            if (response.status == 200) {
+              Swal.fire(
+                'Berhasil!',
+                'Berhasil Checkin!',
+                'success'
+              )
+              fetchAllAbsenDapur();
+            } else if (response.status == 400) {
+              Swal.fire(
+                'Gagal Checkin!',
+                'Anda sudah checkin hari ini!',
+                'error'
+              )
+              fetchAllAbsenDapur();
+            }
+          }
+        });
+      });
+
+      $("#add_checkout").submit(function(e) {
+        e.preventDefault();
+        const fd = new FormData(this);
+        $.ajax({
+          url: '{{ route('checkoutDapur') }}',
+          method: 'post',
+          data: fd,
+          cache: false,
+          contentType: false,
+          processData: false,
+          dataType: 'json',
+          success: function(response) {
+            if (response.status == 200) {
+              Swal.fire(
+                'Berhasil!',
+                'Berhasil Checkout!',
+                'success'
+              )
+              fetchAllAbsenDapur();
+            } else if (response.status == 400) {
+              Swal.fire(
+                'Gagal Checkout!',
+                'Anda sudah checkout hari ini!',
+                'error'
+              )
+              fetchAllAbsenDapur();
+            }
+          }
+        });
+      });
+
+      var date = new Date();
+
+      $('.input-daterange').datepicker({
+        todayBtn: 'linked',
+        format: 'yyyy-mm-dd',
+        autoclose: true
+      });
+
+      var _token = $('input[name="_token"]').val();
+      // fetch all makanans ajax request
+      fetchAllAbsenDapur();
+
+      function fetchAllAbsenDapur() {
+        $.ajax({
+          url: '{{ route('fetchAllAbsenDapur') }}',
+          method: 'get',
+          success: function(response) {
+            $("#show_all_absen").html(response);
+            $("table").DataTable({
+              order: [0, 'desc'],
+              responsive: true
+            });
+          }
+        });
+      }
+
+      function fetchAllAbsenDapurx(from_date = '', to_date = '')
+      {
+        $.ajax({
+        url:"{{ route('fetchAllAbsenDapurx') }}",
+        method:"POST",
+        data:{from_date:from_date, to_date:to_date, _token:_token},
+        dataType:"json",
+        success:function(data)
+        {
+          var output = '';
+          for(var count = 0; count < data.length; count++)
+          {
+          output += '<tr>';
+          output += '<td>' + data[count].id + '</td>';
+          output += '<td>' + data[count].name + '</td>';
+          output += '<td>' + data[count].level + '</td>';
+          output += '<td>' + data[count].created_at + '</td>';
+          output += '<td>' + data[count].checkout + '</td></tr>';
+          }
+          $('tbody').html(output);
+              }
+              })
+      }
+
+
+    $('#filter').click(function(){
+    var from_date = $('#from_date').val();
+    var to_date = $('#to_date').val();
+    if(from_date != '' &&  to_date != '')
+    {
+      $('#table').DataTable().destroy();
+      fetchAllAbsenDapurx(from_date, to_date);
+    }
+    else
+    {
+    alert('Both Date is required');
+    }
+  });
+
+  $('#refresh').click(function(){
+    $('#from_date').val('');
+    $('#to_date').val('');
+    fetchAllAbsenDapur();
+  });
+
+});
+  </script>
+</body>
+
+@endsection
